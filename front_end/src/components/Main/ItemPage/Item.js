f604c4f950ceb9eaf76e018b3d4f09679c89ab23
Craxy_Jesi <39983328+jesintharnold@users.noreply.github.com>
2021-10-24 01:07:38 +0530

    MainPage Items added

---
+++ front_end/src/components/Main/ItemPage/Item.js
@@ -0,0 +1,103 @@
+import './Item.scss';
+
+function Item(){
+return (
+    <div className="Item">
+
+      <>
+      <div className="Item_name">Fruit and vegetables</div>  
+      <div className="Item_list">
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+      </div>
+
+      <div className="Item_name">Fruit and vegetables</div>  
+      <div className="Item_list">
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+          <button className="Item_btn">
+          <span>Chicken leg box</span>
+          <span class="material-icons">add</span>
+          </button>
+
+      </div>
+      </>
+
+    </div>
+)
+}
+
+export default Item;
--- front_end/src/components/Main/MainItems.js
+++ front_end/src/components/Main/MainItems.js
@@ -1,10 +1,12 @@
 import Header from '../Main/TitleSearch/header';
+import Item from './ItemPage/Item';
 import "./MainItems.scss";
 
 function Mainitem(){
     return (
         <div className="Main_items">
         <Header/>
+        <Item/>
         </div>
     )
 }
