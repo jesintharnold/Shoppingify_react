ce2ada7e8b8170afde27122f79708a5269e81974
Craxy_Jesi <39983328+jesintharnold@users.noreply.github.com>
2022-01-17 00:47:36 +0530

    UI  Google-Auth Added

--- front_end/src/Auth/Auth.js
+++ front_end/src/Auth/Auth.js
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom";
 import hearts from '../Assests/heart.svg';
 
 
+
+function googleOauth(){
+  const _url="https://accounts.google.com/o/oauth2/v2/auth";
+  const options = {
+    redirect_uri: process.env.REACT_APP_REDIRECT_URL,
+    client_id: process.env.REACT_APP_CLIENTID,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+  };
+  const qs = new URLSearchParams(options);
+  return `${_url}?${qs.toString()}`;
+};
+
 function Auth() {
   return ReactDOM.createPortal(
   
@@ -13,12 +31,12 @@ function Auth() {
       Shoppingify
     </h5>
 
-  <div className="google-btn" onClick={()=>console.log(`Hello this is Big Bang theory`)}>
+  <a className="google-btn" rel="noreferrer" onClick={()=>console.log(`Hello this is Big Bang theory`)} href={googleOauth()}>
   <div className="google-icon-wrapper">
-    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='Not Found'/>
+    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='Not Found'/>
   </div>
   <p className="btn-text">Login with google</p>
-</div>
+</a>
 
     </div>
  </div>
